ebd81a7ccc09134b5cc5b8a8bc95ceda1a19148e
Marton Lederer <marton@lederer.hu>
2021-02-09 10:59:09 +0100

    chore: basic react template

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import replace from "@rollup/plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import typescript from "rollup-plugin-typescript2";
+import babel from "rollup-plugin-babel";
+import postcss from "rollup-plugin-postcss";
+
+const production = process.env.NODE_ENV === "production";
+
+export default {
+  input: "src/index.tsx",
+  output: {
+    file: "public/build/bundle.js",
+    format: "iife"
+  },
+  plugins: [
+    replace({
+      "process.env.NODE_ENV": JSON.stringify(
+        production ? "production" : "development"
+      )
+    }),
+    resolve({
+      browser: true,
+      extensions: [".js", ".ts", ".tsx", ".jsx"]
+    }),
+    postcss({
+      modules: true
+    }),
+    commonjs(),
+    babel({
+      exclude: ["node_modules/**"]
+    }),
+    typescript({
+      sourceMap: !production
+    }),
+    production && terser(),
+    !production && serve("public"),
+    !production &&
+      livereload({
+        watch: "public"
+      })
+  ],
+  watch: {
+    clearScreen: false
+  }
+};
