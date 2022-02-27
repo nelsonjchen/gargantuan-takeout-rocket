85b007e7fda21537470942cec747be459247fa40
Nelson Chen <crazysim@gmail.com>
2022-02-27 11:18:48 -0800

    ok this might work

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,6 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  const resp = await fetch("https://put-block-from-url-esc-issue-demo-server-3vngqvvpoq-uc.a.run.app/red%2Fblue.txt");
+  const text = await resp.text();
+
+  return new Response(`I got the string: ${text}`);
 }
