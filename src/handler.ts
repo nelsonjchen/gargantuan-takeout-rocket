3461c7308b3bb2a13c1afe303c3c31d573ebe778
Nelson Chen <crazysim@gmail.com>
2022-02-27 11:28:44 -0800

    i don't need processing at all

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,7 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  const resp = await fetch("https://put-block-from-url-esc-issue-demo-server-3vngqvvpoq-uc.a.run.app/red%2Fblue.txt");
-  const text = await resp.text();
+  const resp = await fetch(
+    'https://put-block-from-url-esc-issue-demo-server-3vngqvvpoq-uc.a.run.app/red%2Fblue.txt',
+  )
 
-  return new Response(`I got the string: ${text}`);
+  return resp
 }
