9bc86acfe3fc660333f6dc323719bc66bd7331b7
Nelson Chen <crazysim@gmail.com>
2022-02-27 11:31:01 -0800

    try 1gb file

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,5 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  const resp = await fetch(
-    'https://put-block-from-url-esc-issue-demo-server-3vngqvvpoq-uc.a.run.app/red%2Fblue.txt',
-  )
+  const resp = await fetch('https://speed.hetzner.de/1GB.bin')
 
   return resp
 }
