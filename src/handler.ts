b0c30cce5f6f2c050939bf3608406694e057323c
Nelson Chen <crazysim@gmail.com>
2022-02-27 11:52:11 -0800

    5gb max on azure

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,12 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  const originalResponse = await fetch('https://speed.hetzner.de/10GB.bin')
+  const originalResponse = await fetch('https://speed.hetzner.de/1GB.bin')
   let response = new Response(originalResponse.body, {
     status: 200,
     headers: originalResponse.headers,
   })
   response.headers.append(
     'Content-Disposition',
-    'attachment; filename="10GB.bin"',
+    'attachment; filename="1GB.bin"',
   )
   return response
 }
