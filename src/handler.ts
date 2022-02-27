c2633a91e903261a1a923d7077fee41d8febc74b
Nelson Chen <crazysim@gmail.com>
2022-02-27 11:40:23 -0800

    mutate

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,9 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  const resp = await fetch('https://speed.hetzner.de/1GB.bin')
-
-  return resp
+  const originalResponse = await fetch('https://speed.hetzner.de/1GB.bin')
+  let response = new Response(originalResponse.body, {
+    status: 200,
+    headers: originalResponse.headers,
+  });
+  response.headers.append('Content-Disposition', 'attachment; filename="1GB.bin"')
+  return response
 }
