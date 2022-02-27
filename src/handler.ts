397541c4260db24ff3c7dd9224426987943286ba
Nelson Chen <crazysim@gmail.com>
2022-02-27 11:41:09 -0800

    10 try

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,12 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  const originalResponse = await fetch('https://speed.hetzner.de/1GB.bin')
+  const originalResponse = await fetch('https://speed.hetzner.de/10GB.bin')
   let response = new Response(originalResponse.body, {
     status: 200,
     headers: originalResponse.headers,
-  });
-  response.headers.append('Content-Disposition', 'attachment; filename="1GB.bin"')
+  })
+  response.headers.append(
+    'Content-Disposition',
+    'attachment; filename="10GB.bin"',
+  )
   return response
 }
