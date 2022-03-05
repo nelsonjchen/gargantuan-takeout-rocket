6302e6a072aa87be82e908318005424f27424589
Nelson Chen <crazysim@gmail.com>
2022-03-05 12:19:55 -0800

    wip

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,30 @@
+import { decode } from '../node_modules/@cfworker/base64url/src/decode'
+
 export async function handleRequest(request: Request): Promise<Response> {
-  const originalResponse = await fetch('https://speed.hetzner.de/1GB.bin')
+  const url = new URL(request.url)
+
+  // Check if the URL matches the path desired. If not, just redirect to GitHub
+  if (url.pathname == '/') {
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: 'https://github.com',
+      },
+    })
+  }
+
+  // Decode URL from base64
+
+  const decoded_url = decode(url.pathname.substring(2))
+
+  const originalResponse = await fetch(decoded_url)
   let response = new Response(originalResponse.body, {
     status: 200,
     headers: originalResponse.headers,
   })
-  response.headers.append(
-    'Content-Disposition',
-    'attachment; filename="1GB.bin"',
-  )
+  // response.headers.append(
+  //   'Content-Disposition',
+  //   'attachment; filename="1GB.bin"',
+  // )
   return response
 }
--- test/handler.test.ts
+++ test/handler.test.ts
@@ -1,6 +1,8 @@
 import { handleRequest } from '../src/handler'
 import makeServiceWorkerEnv from 'service-worker-mock'
 
+import { encode } from '../node_modules/@cfworker/base64url/src'
+
 declare var global: any
 
 describe('handle', () => {
@@ -10,9 +12,17 @@ describe('handle', () => {
   })
 
   test('handle GET', async () => {
-    const result = await handleRequest(new Request('/', { method: 'GET' }))
+    const encoded_url = encode('http://icanhazip.com/')
+    const result = await handleRequest(
+      new Request(`/p/${encoded_url}`, { method: 'GET' }),
+    )
     expect(result.status).toEqual(200)
     const text = await result.text()
     expect(text).toEqual('request method: GET')
+  })
+
+  test('handle root get', async () => {
+    const result = await handleRequest(new Request(`/`, { method: 'GET' }))
+    expect(result.status).toEqual(302)
   })
 })
