6599c507a7c877c8f2b50f4c508a6c95aa6eb3f1
Nelson Chen <crazysim@gmail.com>
2022-03-05 23:39:24 -0800

    Allow access to the test server as well

--- src/handler.ts
+++ src/handler.ts
@@ -4,17 +4,17 @@ export async function handleRequest(request: Request): Promise<Response> {
   const url = new URL(request.url)
 
   // Check if the URL matches the path desired. If not, just redirect to GitHub
-  if (url.pathname == '/') {
+  // for project information
+  if (url.pathname == '/' || !url.pathname.startsWith('/p/')) {
     return new Response(null, {
       status: 302,
       headers: {
-        Location: 'https://github.com',
+        Location: 'https://github.com/nelsonjchen/gtr-proxy',
       },
     })
   }
 
   // Decode URL from base64
-
   const base64Url = url.pathname.substring(3)
   const decoded_argument = atob(base64Url)
   if (decoded_argument == null) {
@@ -24,15 +24,24 @@ export async function handleRequest(request: Request): Promise<Response> {
   }
 
   // Check if the URL is a valid URL
+  let decoded_url: URL
   try {
-    const url = new URL(decoded_argument)
+    decoded_url = new URL(decoded_argument)
   } catch (_) {
     return new Response('invalid URL', {
       status: 500,
     })
   }
 
-  const originalResponse = await fetch(decoded_argument, {
+  if (
+    !(validGoogleTakeoutUrl(decoded_url) || validTestServerURL(decoded_url))
+  ) {
+    return new Response('url is not a google takeout or test server url', {
+      status: 403,
+    })
+  }
+
+  const originalResponse = await fetch(decoded_url.toString(), {
     method: request.method,
     headers: request.headers,
   })
@@ -43,4 +52,16 @@ export async function handleRequest(request: Request): Promise<Response> {
   })
 
   return response
+}
+
+export function validTestServerURL(url: URL): boolean {
+  // https://github.com/nelsonjchen/put-block-from-url-esc-issue-demo-server/
+  return url.hostname.endsWith('3vngqvvpoq-uc.a.run.app')
+}
+
+export function validGoogleTakeoutUrl(url: URL): boolean {
+  return (
+    url.hostname.endsWith('apidata.googleusercontent.com') &&
+    url.pathname.startsWith('/download/storage/v1/b/dataliberation/o/')
+  )
 }
--- test/handler.test.ts
+++ test/handler.test.ts
@@ -1,25 +1,43 @@
-import { handleRequest } from '../src/handler'
+import { handleRequest, validGoogleTakeoutUrl } from '../src/handler'
 
 import { btoa } from 'abab'
 
+// URL is too long, just move it to another file.
+import real_url from './real_url'
+
 describe('handle', () => {
-  beforeEach(() => {
-    jest.resetModules()
+  test('has a function that can determine if a URL is from takeout or not', async () => {
+    const bad_url = new URL('http://iscaliforniaonfire.com/')
+    expect(validGoogleTakeoutUrl(bad_url)).toBeFalsy()
+    expect(validGoogleTakeoutUrl(real_url)).toBeTruthy()
   })
 
-  test('handle GET', async () => {
+  test('does not handle non-takeout URL', async () => {
     const encoded_url = btoa('http://iscaliforniaonfire.com/')
     const request_url = `https://example.com/p/${encoded_url}`
     console.debug(request_url)
     const result = await handleRequest(
       new Request(request_url, { method: 'GET' }),
     )
-    // California is always on fire.
-    expect(await result.text()).toEqual(expect.stringContaining('Yes'))
+
+    expect(result.status).toEqual(403)
+  })
+
+  test('handle a real google takeout url that is expired', async () => {
+    const encoded_url = btoa(real_url.toString())
+    const request_url = `https://example.com/p/${encoded_url}`
+    console.debug(request_url)
+    const result = await handleRequest(
+      new Request(request_url, { method: 'GET' }),
+    )
+
+    expect(await result.text()).toEqual(
+      expect.stringContaining('Locked Domain'),
+    )
     expect(result.status).toEqual(200)
   })
 
-  test('handle root get', async () => {
+  test('redirect all other urls to somewhere else, like GitHub maybe', async () => {
     const result = await handleRequest(
       new Request(`https://example.com/`, { method: 'GET' }),
     )
