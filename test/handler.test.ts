86b10db7e239fd11b09f72f668412f6acd9a15e9
Nelson Chen <crazysim@gmail.com>
2022-03-05 21:55:32 -0800

    Working Test

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -2,26 +2,27 @@ import { handleRequest } from '../src/handler'
 
 import { btoa } from 'abab'
 
-declare var global: any
-
 describe('handle', () => {
   beforeEach(() => {
     jest.resetModules()
   })
 
   test('handle GET', async () => {
-    const encoded_url = btoa('http://icanhazip.com/')
+    const encoded_url = btoa('http://iscaliforniaonfire.com/')
+    const request_url = `https://example.com/p/${encoded_url}`
+    console.debug(request_url)
     const result = await handleRequest(
-      new Request(`https://example.com/p/${encoded_url}`, { method: 'GET' }),
+      new Request(request_url, { method: 'GET' }),
     )
-    expect(await result.text()).toBe('test')
+    // California is always on fire.
+    expect(await result.text()).toEqual(expect.stringContaining('Yes'))
     expect(result.status).toEqual(200)
-    const text = await result.text()
-    expect(text).toEqual('request method: GET')
   })
 
   test('handle root get', async () => {
-    const result = await handleRequest(new Request(`https://example.com/`, { method: 'GET' }))
+    const result = await handleRequest(
+      new Request(`https://example.com/`, { method: 'GET' }),
+    )
     expect(result.status).toEqual(302)
   })
 })
