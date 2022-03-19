dc6439019392ef41b34394efa5933cd122456445
Nelson Chen <crazysim@gmail.com>
2022-03-19 10:00:01 -0700

    initial transload test ok

--- test/transload.test.ts
+++ test/transload.test.ts
@@ -25,5 +25,5 @@ describe("transload", () => {
 
   test("can transload a test file from a linux iso mirror directly to azure", async () => {
     await transload(ubuntuIso, process.env.AZ_BLOB_SAS_URL!);
-  });
+  }, 30000);
 });
