ce881f430ab6ead2dfbe9996742cd5b5999a6e53
Nelson Chen <crazysim@gmail.com>
2022-04-17 00:11:29 -0700

    don't minify to make review easier

--- esbuild.js
+++ esbuild.js
@@ -4,7 +4,7 @@ esbuild
   .build({
     entryPoints: ["./src/background.ts", "./src/popup.tsx"],
     bundle: true,
-    // Let's not minify to make the
+    // Let's not minify to make the review easier
     minify: false,
     sourcemap: process.env.NODE_ENV !== "production",
     target: ["chrome58", "firefox57"],
