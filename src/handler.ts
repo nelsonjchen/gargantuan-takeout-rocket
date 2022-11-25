a236e29b9a9de74472defc3ff420cde31a6bf57d
Nelson Chen <crazysim@gmail.com>
2022-11-25 08:36:54 +0000

    Initial implementation

--- src/handler.ts
+++ src/handler.ts
@@ -4,23 +4,15 @@ import { proxyPathnameToAzBlobSASUrl } from './azb'
 export async function handleRequest(request: Request): Promise<Response> {
   const url = new URL(request.url)
 
-  if (url.pathname.startsWith('/p/')) {
-    return handleTakeoutRequest(request)
-  }
-
-  if (url.pathname.startsWith('/p-azb/')) {
-    return handleAzBlobRequest(request)
-  }
-
   if (url.pathname.startsWith('/t-azb/')) {
-    return handleAzBlobRequest(request)
+    return handleTransloadAzBlobRequest(request)
   }
 
   if (url.pathname.startsWith('/version/')) {
     return new Response(
       JSON.stringify(
         {
-          apiVersion: '1.0.0',
+          apiVersion: '2.0.0',
         },
         null,
         2,
@@ -44,84 +36,6 @@ export async function handleRequest(request: Request): Promise<Response> {
   })
 }
 
-export async function handleTakeoutRequest(
-  request: Request,
-): Promise<Response> {
-  const url = new URL(request.url)
-
-  // Decode URL from base64
-  const base64strMatches =
-    /\/p\/((?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?)/.exec(
-      url.pathname,
-    )
-  if (!base64strMatches) {
-    return new Response('could not find base64 url', {
-      status: 500,
-    })
-  }
-  const base64Url = base64strMatches[1]
-  const decoded_argument = atob(base64Url)
-  if (decoded_argument == null) {
-    return new Response('invalid base64', {
-      status: 500,
-    })
-  }
-
-  // Check if the URL is a valid URL
-  let decoded_url: URL
-  try {
-    decoded_url = new URL(decoded_argument)
-  } catch (_) {
-    return new Response('invalid URL', {
-      status: 500,
-    })
-  }
-
-  if (
-    !(validGoogleTakeoutUrl(decoded_url) || validTestServerURL(decoded_url))
-  ) {
-    return new Response(
-      'encoded url was not a google takeout or test server url',
-      {
-        status: 403,
-      },
-    )
-  }
-
-  const originalResponse = await fetch(decoded_url.toString(), {
-    method: request.method,
-    headers: request.headers,
-  })
-
-  const response = new Response(originalResponse.body, {
-    status: originalResponse.status,
-    headers: originalResponse.headers,
-  })
-
-  return response
-}
-
-export async function handleAzBlobRequest(request: Request): Promise<Response> {
-  const url = new URL(request.url)
-  try {
-    const azUrl = proxyPathnameToAzBlobSASUrl(url)
-    const originalResponse = await fetch(azUrl.toString(), {
-      method: request.method,
-      headers: request.headers,
-    })
-
-    const response = new Response(originalResponse.body, {
-      status: originalResponse.status,
-      headers: originalResponse.headers,
-    })
-
-    return response
-  } catch {
-    return new Response('invalid URL', {
-      status: 500,
-    })
-  }
-}
 
 export async function handleTransloadAzBlobRequest(request: Request): Promise<Response> {
   // Check for a 'x-gtr-copy-source' header
@@ -144,7 +58,14 @@ export async function handleTransloadAzBlobRequest(request: Request): Promise<Re
     })
   }
   // Get a readable stream of the original 
-
+  const body = copySourceResponse.body
+  // Return an error if body isn't a ReadableStream
+  if (!(body instanceof ReadableStream)) {
+    return new Response('body is not a ReadableStream', {
+      status: 500,
+    })
+  }
+  
   
   const url = new URL(request.url)
   try {
@@ -153,6 +74,7 @@ export async function handleTransloadAzBlobRequest(request: Request): Promise<Re
     const originalResponse = await fetch(azUrl.toString(), {
       method: request.method,
       headers: request.headers,
+      body
     })
 
     const response = new Response(originalResponse.body, {
@@ -161,8 +83,13 @@ export async function handleTransloadAzBlobRequest(request: Request): Promise<Re
     })
 
     return response
-  } catch e {
-    return new Response('invalid URL', {
+  } catch (e) {
+    if (e instanceof Error) {
+      return new Response(e.toString(), {
+        status: 500,
+      })
+    }
+    return new Response('unknown error', {
       status: 500,
     })
   }
