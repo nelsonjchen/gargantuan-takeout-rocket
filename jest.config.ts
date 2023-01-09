95cd7b3c0411da02b33c3c306db0dda4f32f521f
Nelson Chen <crazysim@gmail.com>
2023-01-08 16:41:03 -0800

    Restore jest to work on windows too

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,8 @@
-import type { Config } from '@jest/types'
+import type { JestConfigWithTsJest } from 'ts-jest'
 
-const config: Config.InitialOptions = {
-  testEnvironment: "miniflare",
-  preset: 'ts-jest/presets/default-esm',
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
+
+const config: JestConfigWithTsJest = {
+  preset: "ts-jest/presets/default-esm",
   transform: {
     '^.+\\.(t|j)sx?$': [
       'ts-jest', {
@@ -13,14 +10,20 @@ const config: Config.InitialOptions = {
       }
     ]
   },
-  testRegex: '/test/.*\\.test\\.ts$',
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  testEnvironment: "miniflare",
+  testEnvironmentOptions: {
+    // Miniflare doesn't yet support the `main` field in `wrangler.toml` so we
+    // need to explicitly tell it where our built worker is. We also need to
+    // explicitly mark it as an ES module.
+    scriptPath: "dist/worker.mjs",
+    modules: true,
+  }
+};
 
-  collectCoverageFrom: ['src/**/*.{ts,js}'],
-  verbose: true,
-  // Load dotenv
-  setupFiles: [
-    'dotenv/config',
-  ]
-}
 
+// noinspection JSUnusedGlobalSymbols
 export default config
