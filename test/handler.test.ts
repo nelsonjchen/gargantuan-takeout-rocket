8be850ba10b0141c09361b9eee36a1dac5c1dada
Nelson Chen <crazysim@gmail.com>
2023-02-05 19:24:07 -0800

    Tests for proxy parts

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -185,6 +185,83 @@ describe('azure proxy handler', () => {
     expect(result.status).toEqual(403)
   })
 
+  test('handles proxying a transload request to azure with azure transloading from cloudflare itself', async () => {
+    // Not exactly a clean unit test since it depends on a properly deployed proxy already, but it'll do.
+    const AZ_STORAGE_TEST_URL_SEGMENT = process.env.AZ_STORAGE_TEST_URL_SEGMENT
+    if (!AZ_STORAGE_TEST_URL_SEGMENT) {
+      throw new Error(
+        'AZ_STORAGE_TEST_URL_SEGMENT environment variable is not set',
+      )
+    }
+
+    const file_source_url = file_test_small_url
+
+    const base_request_url = new URL(
+      `https://example.com/p-azb/${AZ_STORAGE_TEST_URL_SEGMENT}`,
+    )
+    // Change filename of request URL
+    base_request_url.pathname = base_request_url.pathname.replace(
+      'test.dat',
+      'p-azb-transload-direct.dat',
+    )
+
+    // Do a single block upload
+    const single_block_request = new Request(base_request_url, {
+      method: 'PUT',
+      headers: {
+        'x-ms-blob-type': 'BlockBlob',
+        'x-ms-copy-source': file_source_url.toString(),
+      }
+    })
+
+    const single_block_result = await handleRequest(single_block_request)
+    const single_block_ok = await single_block_result.text()
+    expect(single_block_ok).toEqual('')
+  })
+
+  test('handles proxying a transload request to azure with azure transloading from cloudflare via the proxy', async () => {
+    // Not exactly a clean unit test since it depends on a properly deployed proxy already, but it'll do.
+    const AZ_STORAGE_TEST_URL_SEGMENT = process.env.AZ_STORAGE_TEST_URL_SEGMENT
+    if (!AZ_STORAGE_TEST_URL_SEGMENT) {
+      throw new Error(
+        'AZ_STORAGE_TEST_URL_SEGMENT environment variable is not set',
+      )
+    }
+
+    // Construct the file_source_url that is proxied to the proxy
+    // "https://gtr-proxy.677472.xyz/p/" is prepended to the file_test_small_url with file_test_small_url's scheme removed.
+    const file_source_url = new URL(
+      `https://gtr-proxy.677472.xyz/p/${file_test_large_url.toString().replace(
+        'https://',
+        ''
+      )}`,
+    )
+
+
+    const base_request_url = new URL(
+      `https://example.com/p-azb/${AZ_STORAGE_TEST_URL_SEGMENT}`,
+    )
+    // Change filename of request URL
+    base_request_url.pathname = base_request_url.pathname.replace(
+      'test.dat',
+      'p-azb-transload-via-proxy.dat',
+    )
+
+    // Do a single block upload
+    const single_block_request = new Request(base_request_url, {
+      method: 'PUT',
+      headers: {
+        'x-ms-blob-type': 'BlockBlob',
+        'x-ms-copy-source': file_source_url.toString(),
+      }
+    })
+
+    const single_block_result = await handleRequest(single_block_request)
+    const single_block_ok = await single_block_result.text()
+    expect(single_block_ok).toEqual('')
+  })
+
+
 })
 
 describe('takeout proxy handler', () => {
