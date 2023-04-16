042204a6b521d82383d5469832a567e7256fee82
Nelson Chen <crazysim@gmail.com>
2023-04-15 18:03:07 -0700

    Update all dependencies

--- esbuild.js
+++ esbuild.js
@@ -1,7 +1,7 @@
 const esbuild = require("esbuild");
 
 esbuild
-  .build({
+  .context({
     entryPoints: ["./src/background.ts", "./src/popup.tsx"],
     bundle: true,
     // Let's not minify to make the review easier
@@ -9,9 +9,19 @@ esbuild
     sourcemap: process.env.NODE_ENV !== "production",
     target: ["chrome58", "firefox57"],
     outdir: "./public/build",
-    watch: process.env.ESB_WATCH == "true",
     define: {
       "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`
+    }
+  })
+  .then((context) => {
+    if (process.env.ESB_WATCH === "true") {
+      // Enable watch mode
+      context.watch();
+    } else {
+      // Build once and exit if not in watch mode
+      context.rebuild().then((result) => {
+        context.dispose();
+      });
     }
   })
   .catch(() => process.exit(1));
