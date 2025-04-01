bde779d1364f923387c9a68e4277c53415c63b6a
Nelson Chen <crazysim@gmail.com>
2025-03-31 22:40:40 -0700

    Improve error messages for invalid azblob URLs to specify missing account, container, or blob names

--- src/azb.ts
+++ src/azb.ts
@@ -3,15 +3,15 @@ export function azBlobSASUrlToProxyPathname(azb_url: URL, base: string): URL {
   const url_parts = azb_url.pathname.split('/')
   const account_name = hostname_parts[0]
   if (!account_name) {
-    throw new Error('invalid azblob url')
+    throw new Error('invalid azblob url, no account name')
   }
   const container_name = url_parts[1]
   if (!container_name) {
-    throw new Error('invalid azblob url')
+    throw new Error('invalid azblob url, no container name')
   }
   const blob_name = url_parts.slice(2).join('/')
   if (!blob_name) {
-    throw new Error('invalid azblob url')
+    throw new Error('invalid azblob url, no blob name')
   }
 
   const query_params = azb_url.searchParams.toString()
