9c4252d82f3a33c26f24ef31cea7a3e590b92c3c
Nelson Chen <crazysim@gmail.com>
2025-10-26 17:25:34 -0700

    Hello World + 1 in test

--- extension/test/transload.test.ts
+++ extension/test/transload.test.ts
@@ -1,7 +1,17 @@
 import { describe, test, expect, vi } from "vitest";
 
+const PROXY_URL = "https://gtr-proxy.677472.xyz/";
+
 describe("hello world", () => {
   test("it works", () => {
     expect(1 + 1).toBe(2);
   });
+
+  test("get version from PROXY_URL", async () => {
+    const resp = await fetch(`${PROXY_URL}version/`);
+    expect(resp.status).toBe(200);
+    const json = await resp.json();
+    expect(json).toEqual({ apiVersion: '2.0.0' });
+  });
+
 });
