09a171c35d6a633c61335185bdc855328f5abac7
Nelson Chen <crazysim@gmail.com>
2025-10-26 17:50:34 -0700

    Take out a lot of unused stuff

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig, UserConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './public/manifest.json'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest }), tsconfigPaths()],
+  plugins: [tsconfigPaths()],
   test: {
   },
 } as UserConfig)
